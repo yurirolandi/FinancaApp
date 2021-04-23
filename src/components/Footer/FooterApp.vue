400e524cbe1ca06c4a6a511d28fbff4a6264ec48
Rolandi <sd.rolandi@gmail.com>
2021-04-22 23:01:21 -0300

    [add] - header and footer

---
+++ src/components/Footer/FooterApp.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-footer v-bind="localAttrs" :padless="padless" dark>
+    <v-card flat tile width="100%" class="primary lighten-1 text-center">
+      <v-card-text>
+        <v-btn v-for="icon in icons" :key="icon" class="mx-4" icon>
+          <v-icon size="24px">
+            {{ icon }}
+          </v-icon>
+        </v-btn>
+      </v-card-text>
+
+      <v-divider></v-divider>
+
+      <v-card-text class="white--text">
+        {{ new Date().getFullYear() }} — <strong>Finança App</strong>
+      </v-card-text>
+    </v-card>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    icons: ["mdi-home", "mdi-email", "mdi-calendar", "mdi-delete"],
+    items: ["default", "absolute", "fixed"],
+    padless: false,
+    variant: "default",
+  }),
+  computed: {
+    localAttrs() {
+      const attrs = {};
+
+      if (this.variant === "default") {
+        attrs.absolute = false;
+        attrs.fixed = false;
+      } else {
+        attrs[this.variant] = true;
+      }
+      return attrs;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.v-footer {
+  padding: 0;
+}
+</style>
