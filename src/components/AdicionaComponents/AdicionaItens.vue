293e98b58c3e6ffc11fe28089ccc413ff1f1fe58
Rolandi <sd.rolandi@gmail.com>
2021-04-25 13:39:35 -0300

    [feat] - estrutura dinamica

--- src/components/AdicionaComponents/AdicionaItens.vue
+++ src/components/AdicionaComponents/AdicionaItens.vue
@@ -121,33 +121,8 @@ export default {
         valueComercio: "",
         valueParcela: "",
       },
-      comercios: ["Mercado", "Lazer", "Casa", "Salão", "Outros"],
-      parcela: [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6,
-        7,
-        8,
-        9,
-        10,
-        11,
-        12,
-        13,
-        14,
-        15,
-        16,
-        17,
-        18,
-        19,
-        20,
-        21,
-        22,
-        23,
-        24,
-      ],
+      
+     
     };
   },
   methods: {
@@ -182,6 +157,8 @@ export default {
   computed: {
     ...mapGetters({
       task: "getProdutosAdicionados",
+      parcela: "getParcelas",
+      comercios: "getComercio",
     }),
   },
 };
