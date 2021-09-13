ba1017a7228df85617af110e0c5b02dd3f43968a
Rolandi <sd.rolandi@gmail.com>
2021-09-12 21:49:52 -0300

    [ feat ] - include navbar and bottom navigation in app

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <template>
   <v-app>
+    <NavBar />
     <v-main>
       <router-view />
     </v-main>
+    <BottomNavigation />
   </v-app>
 </template>
 
 <script>
+import NavBar from "./components/NavBar/NavBar.vue";
+import BottomNavigation from "./components/BottomNavigation/BottomNavigation";
 export default {
   name: "App",
-  components: {},
+  components: {
+    BottomNavigation,
+    NavBar,
+  },
 };
 </script>
