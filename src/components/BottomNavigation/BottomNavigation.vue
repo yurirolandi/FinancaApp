b7cafa42f4789cfa97fb2293955a169b4c3e4ca5
Rolandi <sd.rolandi@gmail.com>
2021-09-12 21:50:13 -0300

    [ feat ] - bottom navigation component

---
+++ src/components/BottomNavigation/BottomNavigation.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-bottom-navigation v-model="value" shift>
+    <v-btn v-for="(item, index) in items" :key="index" router :to="item.route">
+      <span>{{ item.title }}</span>
+
+      <v-icon> {{ item.icon }} </v-icon>
+    </v-btn>
+  </v-bottom-navigation>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    value: 1,
+    items: [
+      { title: "Home", icon: "mdi-home", route: "/" },
+      { title: "Balanço", icon: "mdi-chart-histogram", route: "/grafico" },
+      { title: "Meses", icon: "mdi-calendar-month", route: "/month" },
+      { title: "Perfil", icon: "mdi-account", route: "account" },
+    ],
+  }),
+};
+</script>
+
+<style lang="scss" scoped>
+.v-btn:not(.v-btn--round).v-size--default {
+  height: 56px;
+  min-width: 64px;
+  width: 100%;
+  padding: 0 16px;
+}
+</style>
