5ab3aa0a5f924eef814d99ff542a01bd5f580613
Rolandi <sd.rolandi@gmail.com>
2021-09-14 21:43:59 -0300

    [ fix ] - nome btn

--- src/components/TableCrud/Table.vue
+++ src/components/TableCrud/Table.vue
@@ -70,8 +70,10 @@
 
             <v-card-actions>
               <v-spacer></v-spacer>
-              <v-btn color="blue darken-1" text @click="close"> Cancel </v-btn>
-              <v-btn color="blue darken-1" text @click="save"> Save </v-btn>
+              <v-btn color="blue darken-1" text @click="close">
+                Cancelar
+              </v-btn>
+              <v-btn color="blue darken-1" text @click="save"> Salvar </v-btn>
             </v-card-actions>
           </v-card>
         </v-dialog>
