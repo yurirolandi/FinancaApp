e20959ef50c93ab5d59660314c4f42ec964ae9a7
Rolandi <sd.rolandi@gmail.com>
2021-10-04 21:14:42 -0300

    [ feat ] - componente loading full screen

---
+++ src/components/common/Loading.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="loading">
+    <v-progress-circular indeterminate color="white"></v-progress-circular>
+    <p>Carregando...</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Loading",
+};
+</script>
+
+<style lang="scss" scoped>
+.loading {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  position: absolute;
+  background: #0b0b0bd3;
+  width: 100%;
+  height: 100%;
+  z-index: 10;
+  p {
+    font: 600;
+    font-size: 2rem;
+    color: white;
+    margin: 0.5rem 0;
+  }
+}
+</style>
