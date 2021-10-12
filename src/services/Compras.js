5c26bf5e605faf64365db8d9bbe73bfaab5b1365
Rolandi <sd.rolandi@gmail.com>
2021-10-12 18:09:15 -0300

    [ fix ] - ajustes de url

--- src/services/Compras.js
+++ src/services/Compras.js
@@ -1,23 +1,22 @@
 import { requestAxios } from "./axios";
-const ano = new Date().getFullYear();
 export const ComprasService = {
   get: (id) => {
-    const url = `/items/${id}/${ano}/meses/`;
+    const url = `/items/${id}/mes`;
 
     return requestAxios.get(url).then((response) => response.data);
   },
   create: (body, id) => {
-    const url = `/items/${id}/${ano}/meses/`;
+    const url = `/items/${id}/mes`;
 
     return requestAxios.post(url, body).then((response) => response.data);
   },
   update: (body, id) => {
-    const url = `/items/${id}/${ano}/meses/`;
+    const url = `/items/${id}/mes`;
 
     return requestAxios.post(url, body).then((response) => response.data);
   },
   delete: (body, id) => {
-    const url = `/items/${id}/${ano}/meses/`;
+    const url = `/items/${id}/mes`;
 
     return requestAxios.post(url, body).then((response) => response.data);
   },
